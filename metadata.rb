568621e18c104193fa2c64292e7a06173c719695
Justin Early <jearly@Latitude-E5420.local>
2016-02-26 09:10:12 -0600

    pinning versions of deps

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jearly0@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures Sinatra Framework'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 
-depends          'nginx',   '~> 2.7.6'
-depends          'unicorn', '~> 2.1.0'
+depends          'nginx',   '= 2.7.6'
+depends          'unicorn', '= 2.1.0'
